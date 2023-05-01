c9c12a1af1450b9387a1828535316daff8af4cb9
twopact <artemov9321@mail.ru>
2023-05-02 00:39:48 +0300

    Add file

--- botik.py
+++ botik.py
@@ -6,6 +6,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
@@ -25,6 +26,7 @@ keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time
 videos_button = types.KeyboardButton('Искать видео')
 channel_button = types.KeyboardButton('Искать канал')
 keyboard.add(videos_button, channel_button)
+
 
 # Обрабатываем команду start
 @bot.message_handler(commands=['start'])
@@ -48,7 +50,7 @@ def search(message):
     bot.send_message(message.chat.id, "Вы выбрали \"Искать видео\". \nПожалуйста, подождите, начинаю поиск...")
     video_href = "https://www.youtube.com/results?search_query=" + message.text
     driver.get(video_href)
-    sleep(3)
+    sleep(1)
     videos = driver.find_elements(By.ID, "video-title")
     num_videos = len(videos)
     if num_videos == 0:
@@ -62,7 +64,7 @@ def search(message):
 def search_from_channel(message):
     bot.send_message(message.chat.id, "Вы выбрали \"Искать канал\", пожалуйста, подождите, начинаю поиск...")
     driver.get("https://www.youtube.com/@" + message.text + "/videos")
-    sleep(3)
+    sleep(1)
     videos = driver.find_elements(By.ID, "thumbnail")
     num_videos = len(videos)
     if num_videos == 0:
@@ -85,15 +87,10 @@ def send_videos(message, videos):
 
 # Функция для отправки запрошенного количества видео на канале
 def send_channel_videos(message, videos):
-    # Получаем запрошенное количество видео из сообщения и приводим его к типу int
     count = int(message.text)
-    # Если запрошенное количество видео больше, чем количество найденных, то отправляем сообщение с ошибкой и прекращаем выполнение функции
     if count > len(videos):
         bot.send_message(message.chat.id, f"Количество запрошенных видео ({count}) больше, чем количество найденных ({len(videos)}).")
         return
-    # Отправляем сообщение о том, что начинаем отправлять видео
     bot.send_message(message.chat.id, f"Отправляю {count} видео...")
-    # Отправляем запрошенное количество видео
     for i in range(count):
-        # Находим элементы с видео на странице и отправляем ссылку на i-тое видео
         bot.send_message(message.chat.id, driver.find_elements(By.ID, "dismissible")[i].find_element(By.ID, "thumbnail").get_attribute("href"))
--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ from botik import bot
 if __name__ == "__main__":
     while True:
         try:
-            print("###Bot started!")
+            print("#Bot started!")
             bot.polling(none_stop=True, interval=0)
         except Exception as e:
             print(e)
